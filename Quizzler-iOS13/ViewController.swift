19bd8c8f1c38adfd12998fc9b48eaf89ce739b0b
dbunk903 <dbunk903@gmail.com>
2022-02-27 12:33:38 +0900

    Implement Struct for Question, Refactor existing Code with the Struct

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -12,11 +12,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
-    
+     
     let quiz = [
-        ["Four + Two is equal to Six.","True"],
-        ["Five - Three is greater than One", "True"],
-        ["Three + Eight is less than Ten","False"]
+        Question(q: "Four + Two is equal to Six.", a: "True"),
+        Question(q: "Five - Three is greater than One", a: "True"),
+        Question(q: "Three + Eight is less than Ten", a: "False")
     ]
     
     var questionNumber = 0
@@ -32,7 +32,7 @@ class ViewController: UIViewController {
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         
         let userAnswer = sender.currentTitle // True, False
-        let actualAnswer = quiz[questionNumber][1]
+        let actualAnswer = quiz[questionNumber].answer
         
         if userAnswer == actualAnswer{
             print("Right!!")
@@ -54,7 +54,7 @@ class ViewController: UIViewController {
     }
     
     func updateUI(){
-        questionLabel.text = quiz[questionNumber][0]
+        questionLabel.text = quiz[questionNumber].text
     }
     
 }
